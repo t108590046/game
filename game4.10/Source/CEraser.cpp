a170ef2ce72234aaa631a3db47d6e641e4516561
LAPTOP-638SSQ21\banan <t108590046@ntut.org.tw>
2021-03-17 23:54:21 +0800

    小雞能夠左右擺頭 還沒做走路動畫

---
+++ game4.10/Source/CEraser.cpp
@@ -0,0 +1,122 @@
+#include "stdafx.h"
+#include "Resource.h"
+#include <mmsystem.h>
+#include <ddraw.h>
+#include "audio.h"
+#include "gamelib.h"
+#include "CEraser.h"
+
+namespace game_framework {
+	/////////////////////////////////////////////////////////////////////////////
+	// CEraser: Eraser class
+	/////////////////////////////////////////////////////////////////////////////
+
+	CEraser::CEraser()
+	{
+		Initialize();
+	}
+
+	int CEraser::GetX1()
+	{
+		return x;
+	}
+
+	int CEraser::GetY1()
+	{
+		return y;
+	}
+
+	int CEraser::GetX2()
+	{
+		return x + animation.Width();
+	}
+
+	int CEraser::GetY2()
+	{
+		return y + animation.Height();
+	}
+
+	void CEraser::Initialize()
+	{
+		const int X_POS = 280;
+		const int Y_POS = 400;
+		x = X_POS;
+		y = Y_POS;
+		isMovingLeft = isMovingRight = isMovingUp = isMovingDown = false;
+	}
+
+	void CEraser::LoadBitmap()
+	{
+		//animation.SetDelayCount(50);
+
+		animation.AddBitmap(IDB_CHICKEN1, RGB(34, 177, 76));
+		goToRight.AddBitmap(IDB_CHICKEN1, RGB(34, 177, 76));
+		goToLeft.AddBitmap(IDB_CHICKEN4, RGB(34, 177, 76));
+
+	}
+
+	void CEraser::OnMove()
+	{
+		const int STEP_SIZE = 9;
+		animation.OnMove();
+		goToRight.OnMove();
+		goToLeft.OnMove();
+		if (isMovingLeft)
+			x -= STEP_SIZE;
+		if (isMovingRight)
+			x += STEP_SIZE;
+		if (isMovingUp)
+			y -= STEP_SIZE;
+		if (isMovingDown)
+			y += STEP_SIZE;
+
+	}
+
+	void CEraser::SetMovingDown(bool flag)
+	{
+		isMovingDown = flag;
+	}
+
+	void CEraser::SetMovingLeft(bool flag)
+	{
+		isMovingLeft = flag;
+	}
+
+	void CEraser::SetMovingRight(bool flag)
+	{
+		isMovingRight = flag;
+	}
+
+	void CEraser::SetMovingUp(bool flag)
+	{
+		isMovingUp = flag;
+	}
+
+	void CEraser::SetXY(int nx, int ny)
+	{
+		x = nx; y = ny;
+	}
+
+	void CEraser::OnShow()
+	{
+		if (isMovingRight) {
+			
+			goToRight.SetTopLeft(x, y);
+			goToRight.OnShow();
+			animation.deleteBmp();
+			animation.AddBitmap(IDB_CHICKEN1, RGB(34, 177, 76));
+		}
+		else if(isMovingLeft){
+
+			goToLeft.SetTopLeft(x, y);
+			goToLeft.OnShow();
+			animation.deleteBmp();
+			animation.AddBitmap(IDB_CHICKEN4, RGB(34, 177, 76));
+		}
+		else {
+			animation.SetTopLeft(x, y);
+			animation.OnShow();
+		}
+
+	}
+}
